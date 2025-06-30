5ffd0e6fe32a791d745bc86f23ec38cded1f88e3
vanshikaaa2003 <153658683+vanshikaaa2003@users.noreply.github.com>
2025-06-30 13:00:24 +0530

    Update config.js
    “chore: split openai key”

--- docs/config.js
+++ docs/config.js
@@ -1,4 +1,5 @@
 window.ENV = {
   supabaseUrl: 'https://xqfrwcarronnxwmgehjq.supabase.co',
   supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhxZnJ3Y2Fycm9ubnh3bWdlaGpxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDE3Njc4MjgsImV4cCI6MjA1NzM0MzgyOH0.qruDS9NddI7vnERNvlXkN6hMfDfqYr35f8uPed9kjQc'
+ openAiKey: 'sk-proj-' + '_2BlHSVC0fJvKJwj7EkKXuRQ4EEaqJPpne85D9m74NSo_RN8D8ee2ZR_cTXpL90597IWAtGIXbT3BlbkFJY7nslqmLedpPVJSyL0ObgiOA3UgWVqXDOgo7JtwtA-cQ49y74pP0ZiXoQKDNMa57--e3azaqUA'
 };
