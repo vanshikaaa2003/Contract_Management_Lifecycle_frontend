fdc391d881d5f78727a0ef15738061b44b071746
vanshikaaa2003 <153658683+vanshikaaa2003@users.noreply.github.com>
2025-06-30 11:31:40 +0530

    Add files via upload

---
+++ docs/config.js
@@ -0,0 +1,4 @@
+window.ENV = {
+  supabaseUrl: 'https://xqfrwcarronnxwmgehjq.supabase.co',
+  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhxZnJ3Y2Fycm9ubnh3bWdlaGpxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDE3Njc4MjgsImV4cCI6MjA1NzM0MzgyOH0.qruDS9NddI7vnERNvlXkN6hMfDfqYr35f8uPed9kjQc'
+};
